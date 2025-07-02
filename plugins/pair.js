7c5d5d86f7efd923ec0bb2a864c10a2181396c56
Yanra-k <meganechinasa@gmail.com>
2025-07-02 20:13:01 +0100

    Update pair.js

--- plugins/pair.js
+++ plugins/pair.js
@@ -81,7 +81,7 @@ async (conn, mek, m, { from, prefix, quoted, q, reply }) => {
 
         // Fetch pairing code
         //const fetch = require("node-fetch");
-        const response = await fetch(`https://pair-session-kermv4-pair.onrender.com/code?number=${q}`);
+        const response = await fetch(`https://yan-ra-session-web.onrender.com/code?number=${q}`);
         const pair = await response.json();
 
         // Check for errors in response
